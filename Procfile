ffb3e3b5b18659a44274d25582293624509c05fe
Louis Mullie <louis.mullie@gmail.com>
2014-04-21 15:08:55 -0400

    Add basic functionality

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec thin start -p $PORT
+db: mongod --auth
--- app.rb
+++ app.rb
@@ -4,13 +4,38 @@ Bundler.require
 STDOUT.sync = true
 
 class App < Sinatra::Base
+  
+  require 'mongoid'
+  require 'mongo'
+  
+  Mongoid.load!('./mongoid.yml', :development)
+  
+  class Tenant
+    
+    include Mongoid::Document
+    
+    field :heroku_id, type: String
+    field :plan, type: String
+    field :callback_url, type: String
+    field :options, type: Hash
+    
+    field :username, type: String
+    field :password, type: String
+    field :databases, type: Array
+    
+  end
+  
+  AdminURI = 'mongodb://admin:123456@localhost:27017/admin'
+  Host = 'localhost:27017'
+  
   use Rack::Session::Cookie, secret: ENV['SSO_SALT']
-
+  
   @@resources = []
 
   Resource = Class.new(OpenStruct)
-
+  
   helpers do
+    
     def protected!
       unless authorized?
         response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
@@ -40,7 +65,9 @@ class App < Sinatra::Base
     end
 
     def get_resource
-      @@resources.find {|u| u.id == params[:id].to_i } or halt 404, 'resource not found'
+      Tenant.find(params[:id])
+    rescue
+      halt 404, 'resource not found'
     end
   end
 
@@ -78,34 +105,60 @@ class App < Sinatra::Base
     puts params.inspect
     sso
   end
+  
+  # mongodb://535561fb2193e2edec000001:79922195890938c26d831e69676c6a11@localhost:27017/535561fb2193e2edec000001
 
   # provision
   post '/heroku/resources' do
+    
     show_request
     protected!
+    
     if json_body['region'] != 'amazon-web-services::us-east-1'
       status 422
       body({:error => 'Region is not supported by this provider.'}.to_json)
     end
-    @@resources << resource = Resource.new(:id => @@resources.size + 1,
-                            :heroku_id => json_body['heroku_id'],
-                            :plan => json_body.fetch('plan', 'test'),
-                            :region => json_body['region'],
-                            :callback_url => json_body['callback_url'],
-                            :options => json_body['options'])
+    
+    username = OpenSSL::Random.random_bytes(8).unpack("H*").first
+    password = OpenSSL::Random.random_bytes(18).unpack("H*").first
+    database = OpenSSL::Random.random_bytes(10).unpack("H*").first
+    
+    tenant = Tenant.new(:heroku_id => json_body['heroku_id'],
+                        :plan => json_body.fetch('plan', 'test'),
+                        :region => json_body['region'],
+                        :callback_url => json_body['callback_url'],
+                        :options => json_body['options'],
+                        :username => username,
+                        :password => password,
+                        :databases => [database])
+    
+    client = Mongo::MongoClient.from_uri(AdminURI)
+    
+    db = client[database]
+    db.add_user(username, password, false, { roles: ['dbAdmin', 'readWrite'] })
+    
+    uri = "mongodb://#{username}:#{password}@#{Host}/#{database}"
+    
     status 201
+    
     body({
-      :id => resource.id,
-      :config => {"CRYODEX_URL" => 'http://yourapp.com/user'},
-      :message => 'Dear customer, your addon is now provisioned!'
+      :id => tenant.id.to_s,
+      :config => { "CRYODEX_URL" => uri },
+      :message => 'Your addon is now provisioned!'
     }.to_json)
+    
   end
 
   # deprovision
-  delete '/heroku/resources/:id' do
+  delete '/heroku/resources/:id' do |id|
     show_request
     protected!
-    @@resources.delete(get_resource)
+    
+    client = Mongo::MongoClient.from_uri(AdminURI)
+    client.drop_database(id)
+    
+    get_resource.destroy
+    
     "ok"
   end
 
@@ -115,6 +168,7 @@ class App < Sinatra::Base
     protected!
     resource = get_resource
     resource.plan = json_body['plan']
+    resource.save!
     {}.to_json
   end
   
